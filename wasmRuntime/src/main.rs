5e79ff91215b7209f716ef9f1b15e4ea6f967e97
Dwaha Daud <dwahadaud68@gmail.com>
2024-11-06 15:37:45 -0500

    implemented label cloudcalls

--- wasmRuntime/src/main.rs
+++ wasmRuntime/src/main.rs
@@ -1,6 +1,5 @@
 use core::str;
 use std::env;
-use serde_json;
 use wasmtime::*;
 
 use anyhow::{Result, Error};
@@ -9,7 +8,7 @@ use labeled::{buckle::{Buckle, Component}, Label};
 
 struct RuntimeState {
     current_label: Buckle,
-    privilege: Component
+    current_privilege: Component
 }
 
 fn main() -> Result<()>{
@@ -29,7 +28,7 @@ fn main() -> Result<()>{
     // set up init runtime state and create data store for state
     let init_state = RuntimeState{
         current_label: Buckle::public(),
-        privilege: Component::dc_true()
+        current_privilege: Component::dc_true()
     };
 
     let mut store = Store::new(
@@ -37,27 +36,25 @@ fn main() -> Result<()>{
         init_state
     );
 
+    linker.func_wrap("runtime", "buckleParse", |mut caller: Caller<'_, RuntimeState>, offset: i32, len: i32| -> Result<Rooted<ExternRef>> {
+        let mem = caller.get_export("memory").unwrap().into_memory().unwrap();
+
+        let mut buf = vec![0u8; len as usize];
+        mem.read(&caller, offset as usize, &mut buf).unwrap();
+
+        let input_str = str::from_utf8(&buf).unwrap();
+        let parsed_buckle = Buckle::parse(input_str);
+        
+        if parsed_buckle.is_ok() {
+            ExternRef::new(&mut caller, parsed_buckle.unwrap())
+        } else {
+            ExternRef::new(&mut caller, None::<Buckle>)
+        }
+    })?;
+
     linker.func_wrap("runtime", "getCurrentLabel", |mut caller: Caller<'_, RuntimeState>| -> Result<Rooted<ExternRef>> {
         let cur_label = caller.data().current_label.clone();
         ExternRef::new(&mut caller, cur_label)
-       
-        // println!("Entered getCurrentLabel function:");
-        // let mem = caller.get_export("memory").unwrap().into_memory().unwrap();
-
-        // // generate serialized json string for current label
-        // let label = serde_json::to_string(&caller.data().current_label).unwrap();
-        // println!("The current label is: {:#?}", caller.data().current_label);
-
-        // // write to memory
-        // mem.write(&mut caller, offset as usize, label.as_bytes()).unwrap();
-
-        // let mut buf = vec![0u8; label.len()];
-        // mem.read(&caller, offset as usize, &mut buf).unwrap();
-        // let ser_label = str::from_utf8(&buf).unwrap();
-        // println!("Read serialized label from WebAssembly memory: {}", ser_label);
-        // let des_label: Buckle = serde_json::from_str(ser_label).unwrap();
-        // println!("Deserialized label from WebAssembly memory: {:#?}\n", des_label);
-
     })?;
 
     linker.func_wrap("runtime", "printExternRef", |caller: Caller<'_, RuntimeState>, l: Option<Rooted<ExternRef>>| {
@@ -65,54 +62,48 @@ fn main() -> Result<()>{
             .unwrap()
             .data(&caller).unwrap()
             .downcast_ref::<Buckle>()
-            .ok_or_else(|| Error::msg("externref was not a Buckle")).unwrap()
-            .clone();
+            .ok_or_else(|| Error::msg("printExternRef: input externref not a Buckle")).unwrap()
+        .clone();
 
-        println!("Buckle passed to host: {:#?}", label);
-    })?;
-
-    linker.func_wrap("runtime", "getCurrentLabelSize", |caller: Caller<'_, RuntimeState>| -> i32 {
-        let label = serde_json::to_string(&caller.data().current_label).unwrap();
-
-        label.as_bytes().len() as i32
-    })?;
-
-    linker.func_wrap("runtime", "taintWithLabelSize", |mut caller: Caller<'_, RuntimeState>, offset: i32, len: i32| -> i32 {
-        let mem = caller.get_export("memory").unwrap().into_memory().unwrap();
-
-        let mut buf = vec![0u8; len as usize];
-        mem.read(&caller, offset as usize, &mut buf).unwrap();
-        let input_label_str = str::from_utf8(&buf).unwrap();
-
-        let input_label: Buckle = serde_json::from_str(input_label_str).unwrap();
-        let result_label = caller.data().current_label.clone().lub(input_label);
-
-        serde_json::to_string(&result_label).unwrap().as_bytes().len() as i32
+        println!("Entered printExternRef:");
+        println!("Input label is: {:#?}\n", label);
     })?;
 
     linker.func_wrap("runtime", "taintWithLabel", |mut caller: Caller<'_, RuntimeState>, 
-    src_offset: i32, src_len: i32, dest_offset: i32| {
-
+     input_ref: Option<Rooted<ExternRef>>| -> Result<Rooted<ExternRef>> {
         println!("Entered taintWithLabel function:");
-        println!("Current label is: {:#?}", caller.data().current_label);
-
-        let mem = caller.get_export("memory").unwrap().into_memory().unwrap();
-
-        // read input label from WebAssembly mem and deserialize it
-        let mut buf = vec![0u8; src_len as usize];
-        mem.read(&caller, src_offset as usize, &mut buf).unwrap();
-        let input_label_str = str::from_utf8(&buf).unwrap();
-
-        let input_label: Buckle = serde_json::from_str(input_label_str).unwrap();
+        let input_label = input_ref.unwrap()
+            .data(&caller).unwrap()
+            .downcast_ref::<Buckle>()
+            .ok_or_else(|| Error::msg("taintWithLabel: input externref not a Buckle")).unwrap()
+        .clone();
         println!("Input label is: {:#?}", input_label);
         
         // taint current label with input label
         caller.data_mut().current_label = caller.data().current_label.clone().lub(input_label);
-        println!("After tainting, new current label is: {:#?}\n", caller.data().current_label);
+        println!("After tainting, cur_label is: {:#?}\n", caller.data().current_label);
+        
+        let cur_label = caller.data().current_label.clone();
+        ExternRef::new(&mut caller, cur_label)
+    })?;
 
-        // write tainted current label to dest_offset, returning it
-        let cur_label_ser = serde_json::to_string(&caller.data().current_label).unwrap();
-        mem.write(&mut caller, dest_offset as usize, cur_label_ser.as_bytes()).unwrap();
+    linker.func_wrap("runtime", "declassify", |mut caller: Caller<'_, RuntimeState>, 
+        input_ref: Option<Rooted<ExternRef>>| -> Result<Rooted<ExternRef>> {
+        
+        let target_secrecy = input_ref.unwrap()
+            .data(&caller).unwrap()
+            .downcast_ref::<Component>()
+            .ok_or_else(|| Error::msg("declassify: input externRef not a Component")).unwrap()
+        .clone();
+
+        if (caller.data().current_privilege.clone() & target_secrecy.clone()).implies(&caller.data().current_label.secrecy) {
+            let new_label = Buckle::new(target_secrecy.clone(), caller.data().current_label.integrity.clone());
+            caller.data_mut().current_label = new_label.clone();
+            ExternRef::new(&mut caller, new_label)
+        } else {
+            let cur_label = caller.data().current_label.clone();
+            ExternRef::new(&mut caller, cur_label)
+        }
     })?;
 
     // instantiate module
--- wasmRuntime/test.wat
+++ wasmRuntime/test.wat
@@ -1,10 +1,17 @@
 (module
     (import "runtime" "getCurrentLabel" (func $getCurrentLabel (result externref)))
+    (import "runtime" "buckleParse" (func $buckleParse (param i32) (param i32) (result externref)))
     (import "runtime" "printExternRef" (func $printExternRef (param externref)))
+    (import "runtime" "taintWithLabel" (func $taintWithLabel (param externref) (result externref)))
     (memory (export "memory") 2 3)
-    (data (i32.const 0x1000) "hello hello h")
+    (data (i32.const 0x1000) "Dwaha,Dwaha")
 
     (func (export "run")
+        i32.const 0x1000
+        i32.const 11
+        call $buckleParse
+        call $taintWithLabel
+        drop
         call $getCurrentLabel
         call $printExternRef
     )
