be960188d3b7da9746c8daf98b8ea2713c78dd54
Dwaha Daud <dwahadaud68@gmail.com>
2024-11-15 20:38:12 -0500

    minor change to serde version

--- function/src/lib.rs
+++ function/src/lib.rs
@@ -1,5 +1,6 @@
 use extism_pdk::*;
 use labeled::buckle::{Buckle, Component};
+use faasten_core::faasten_interface_types::{DentOpen, DentOpenResult, DentCreate, dent_create, DentResult};
 
 #[host_fn]
 extern "ExtismHost" {
@@ -7,6 +8,8 @@ extern "ExtismHost" {
     fn buckle_parse(input_str: &str) -> Json<Option<Buckle>>;
     fn taint_with_label(input_label: Json<Buckle>) -> Json<Buckle>;
     fn declassify(target_secrecy: Json<Component>) -> Json<Buckle>;
+    fn dent_open(dent_open_json: Json<DentOpen>) -> Json<DentOpenResult>;
+    fn dent_create(dent_create_json: Json<DentCreate>) -> Json<DentResult>;
 }
 
 
@@ -18,7 +21,13 @@ pub fn run() -> FnResult<String> {
         let Json(label3) = taint_with_label(Json(label2.clone().unwrap())).unwrap();
         let Json(label4) = get_current_label().unwrap();
         let Json(label5) = declassify(Json(Component::dc_true())).unwrap();
-        Ok(format!("1:{:#?}\n2:{:#?}\n3:{:#?}\n4:{:#?}\n5:{:#?}", label1, label2.unwrap(), label3, label4, label5))
+
+        let Json(de_result) = dent_create(Json(DentCreate{label: label2.unwrap(), kind: Some(dent_create::Kind::File)})).unwrap();
+        
+        Ok(format!("1:{:#?}\n2:{:#?}\n3:{:#?}\n4:{:#?}\n5:{:#?}", 
+            label1, 
+            label2.unwrap(), 
+            label3, label4, label5))
     }
 }
 
